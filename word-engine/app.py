87d7d090e8bf72b08d034ff5caae2dda126ccb2e
Michael Descombes <69525888+descomic@users.noreply.github.com>
2025-02-16 15:04:23 +0100

    Return valid random word
    When creating a random word in word engine, check that the word exists
    (has a done empty vector in spacy), if it does not try a new random
    word, else return

--- word-engine/app.py
+++ word-engine/app.py
@@ -3,33 +3,40 @@ import os
 import spacy
 from flask import Flask, request, render_template_string
 from random_word import RandomWords
+from spacy import Language
+from spacy.tokens import Doc
 
-app = Flask(__name__)
-nlp = spacy.load("en_core_web_md")
-random_words = RandomWords()
+app: Flask = Flask(__name__)
+nlp: Language = spacy.load("en_core_web_md")
+random_words: RandomWords = RandomWords()
 
 
 @app.route("/", methods=["GET"])
-def hello():
+def hello() -> str:
     print(f"Received request: {request.url}")
     return render_template_string("Word Engine API")
 
 
 @app.route("/similarity/<word>/<other_word>", methods=["GET"])
-def similarity(word, other_word):
+def similarity(word, other_word) -> str:
     print(f"Received request: {request.url}")
 
-    nlp_word = nlp(word)
-    nlp_other_word = nlp(other_word)
-    score = nlp_word.similarity(nlp_other_word)
+    nlp_word: Doc = nlp(word)
+    nlp_other_word: Doc = nlp(other_word)
+    score: float = nlp_word.similarity(nlp_other_word)
 
     return render_template_string(str(score))
 
 
 @app.route("/random", methods=["GET"])
-def random():
+def random() -> str:
     print(f"Received request: {request.url}")
-    return render_template_string(random_words.get_random_word())
+    random_word = random_words.get_random_word()
+    nlp_word: Doc = nlp(random_word)
+    while not nlp_word or not nlp_word.vector_norm:
+        random_word = random_words.get_random_word()
+        nlp_word = nlp(random_word)
+    return render_template_string(random_word)
 
 
 if __name__ == "__main__":
